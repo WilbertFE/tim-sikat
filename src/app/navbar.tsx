eccb60d8632f9f5404481a87474f159a4da09ffd
WilbertFE <wilbertbernadi699@gmail.com>
2024-09-18 05:54:32 +0700

    Default Configuration

---
+++ src/app/navbar.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import { CircleChevronDown, Menu, X } from "lucide-react";
+import { useState } from "react";
+
+const navbarContent = [
+  {
+    title: "Kenali Kandidat",
+    icon: <CircleChevronDown strokeWidth={1.5} />,
+  },
+  {
+    title: "Kawal SIKAT",
+  },
+  {
+    title: "Kontak",
+  },
+  {
+    title: "Tim SIKAT",
+  },
+  {
+    title: "Program Kerja",
+    icon: <CircleChevronDown strokeWidth={1.5} />,
+  },
+  {
+    title: "Dukung SIKAT",
+  },
+];
+
+const hamburgerContent = [
+  {
+    title: "Kenali Wilbert",
+  },
+  {
+    title: "Kenali Jacob",
+  },
+  {
+    title: "Program Kerja",
+  },
+  {
+    title: "Dukung SIKAT",
+  },
+  {
+    title: "TIM SIKAT",
+  },
+
+  {
+    title: "Kawal SIKAT",
+  },
+  {
+    title: "SIKAT-kan Instagram",
+  },
+  {
+    title: "SIKAT-kan Whatsapp",
+  },
+];
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="fixed left-0 right-0 min-h-12 p-4 shadow-sm">
+      <ul className="hidden lg:flex lg:justify-around lg:p-4">
+        {navbarContent.map((content, i) => (
+          <li key={i}>
+            <HoverCard>
+              <HoverCardTrigger className="text-xl flex items-center gap-x-1 tracking-wide">
+                {content.title}
+                {content.icon ? content.icon : null}
+              </HoverCardTrigger>
+              <HoverCardContent>
+                The React Framework – created and maintained by @vercel.
+              </HoverCardContent>
+            </HoverCard>
+          </li>
+        ))}
+      </ul>
+      <div className="lg:hidden flex items-center shadow-sm p-4">
+        {(isOpen && (
+          <X size={32} className="z-20" onClick={() => setIsOpen(false)} />
+        )) || (
+          <Menu size={32} className="z-20" onClick={() => setIsOpen(true)} />
+        )}
+        <h1 className="text-xl font-bold w-full text-center">SIKAT</h1>
+        {isOpen && (
+          <ul className="absolute px-4 py-24 flex flex-col justify-around min-h-screen left-0 right-0 top-0 bottom-0">
+            {hamburgerContent.map((content, i) => (
+              <li
+                className="text-xl p-2 font-medium flex items-center gap-x-1 tracking-wide"
+                key={i}
+              >
+                {content.title}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </nav>
+  );
+}
